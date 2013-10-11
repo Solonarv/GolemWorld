a8b8eaefe5be3171dfe759be7f48aa8a9a3a42f1
Solonarv <nstamm@gmx.de>
2013-10-11 22:10:43 +0200

    golem txturs r feex'd
    Methods that were being overridden in RenderCustomGolem had been renamed

--- common/com/solonarv/mods/golemworld/golem/RenderCustomGolem.java
+++ common/com/solonarv/mods/golemworld/golem/RenderCustomGolem.java
@@ -2,15 +2,22 @@ package com.solonarv.mods.golemworld.golem;
 
 import net.minecraft.client.renderer.entity.RenderIronGolem;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderCustomGolem extends RenderIronGolem {
     
-    protected ResourceLocation func_110775_a(Entity par1Entity) {
-        return this.func_110898_a((EntityCustomGolem) par1Entity);
+    @Override
+    protected ResourceLocation getEntityTexture(Entity par1Entity) {
+        return this.getGolemTexture((EntityCustomGolem) par1Entity);
     }
     
-    protected ResourceLocation func_110898_a(EntityCustomGolem theGolem) {
+    @Override
+    protected ResourceLocation getIronGolemTextures(EntityIronGolem theGolem) {
+        return this.getGolemTexture((EntityCustomGolem) theGolem);
+    }
+    
+    protected ResourceLocation getGolemTexture(EntityCustomGolem theGolem){
         return theGolem.stats().texture;
     }
 }
